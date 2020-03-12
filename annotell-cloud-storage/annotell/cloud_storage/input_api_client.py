3ab109303c14b3d1524ebfc65d70e92d4be6b0a5
Marko Cotra <cotra.marko@gmail.com>
2020-03-12 14:44:34 +0100

    Add code for input_api_client

--- annotell-cloud-storage/annotell/cloud_storage/input_api_client.py
+++ annotell-cloud-storage/annotell/cloud_storage/input_api_client.py
@@ -2,10 +2,10 @@ import os
 
 import requests
 import logging
-from typing import List, Mapping
+from typing import List, Mapping, Optional
 from pathlib import Path
 import mimetypes
-from . import __version__
+# from . import __version__
 
 BASE_URL = "https://input.annotell.com"
 
@@ -22,7 +22,7 @@ class InputApiClient:
             "Authorization": f"Bearer {self.api_token}",
             "Accept-Encoding": "gzip",
             "Accept": "application/json",
-            "User-Agent": f"annotell-cloud-storage:{__version__}"
+            "User-Agent": f"annotell-cloud-storage:0.2.0"  # FIXME {__version__}
         }
 
     def _raise_on_error(self, resp: requests.Response) -> requests.Response:
@@ -47,6 +47,10 @@ class InputApiClient:
             log.info(f"Uploading file={fi}")
             with fi.open('rb') as f:
                 content_type = mimetypes.guess_type(file)[0]
+                # Hack deluxe
+                # https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
+                if not content_type:
+                    content_type = 'application/octet-stream'
                 headers = {"Content-Type": content_type}
                 resp = self.session.put(upload_url, data=f, headers=headers)
                 try:
@@ -55,22 +59,95 @@ class InputApiClient:
                     log.error(f"Got {resp.status_code} error calling cloud bucket upload, "
                               f"got response\n{resp.content}")
 
-
-    def create_inputs(self, files: List[str], job_id: str):
+    def create_inputs(self, files: List[str], job_id: str, input_list_id: int, metadata: dict):
         """Create inputs from uploaded files"""
         log.info(f"Creating inputs for files with job_id={job_id}")
         url = f"{self.base_url}/v1/inputs"
-        js = dict(files=files, jobId=job_id)
+        js = dict(
+            files=files,
+            internalId=job_id,
+            inputListId=input_list_id,
+            metadata=metadata
+        )
         resp = self.session.post(url, json=js, headers=self.headers)
-        self._raise_on_error(resp)
+        return self._raise_on_error(resp).json()
 
-    def create_inputs_for_files(self, folder: Path, files: List[str]):
+    def create_inputs_for_files(self, folder: Path, files: List[str], input_list_id: int, metadata: dict):
         """Upload files in folder to an input"""
         resp = self.get_upload_urls(files)
         items = resp['items']
         job_id = resp['jobId']
         files = list(items.keys())
         self.upload_files(folder, items)
-        self.create_inputs(files, job_id)
-        return job_id
+        resp = self.create_inputs(files, job_id, input_list_id, metadata)
+        return resp
 
+    def mend_input_data(self):
+        url = f"{self.base_url}/v1/inputs/mend-input-metadata"
+        resp = self.session.get(url, headers=self.headers)
+        return self._raise_on_error(resp).json()
+
+    def invalidate_input(self):
+        url = f"{self.base_url}/v1/inputs/invalidate"
+        resp = self.session.get(url, headers=self.headers)
+        return self._raise_on_error(resp).json()
+
+    def list_projects(self):
+        url = f"{self.base_url}/v1/inputs/projects"
+        resp = self.session.get(url, headers=self.headers)
+        return self._raise_on_error(resp).json()
+
+    def list_input_lists(self, project_id: int):
+        url = f"{self.base_url}/v1/inputs/input-lists?projectId={project_id}"
+        resp = self.session.get(url, headers=self.headers)
+        return self._raise_on_error(resp).json()
+
+    def get_calibration_data(self, id: Optional[int] = None, external_id: Optional[str] = None):
+        url = f"{self.base_url}/v1/inputs/calibration-data"
+        if id:
+            url += f"?id={id}"
+        elif external_id:
+            url += f"?externalId={external_id}"
+
+        resp = self.session.get(url, headers=self.headers)
+        return self._raise_on_error(resp).json()
+
+    def create_calibration_data(self, calibration: dict, external_id: str):
+        url = f"{self.base_url}/v1/inputs/calibration-data"
+        js = dict(
+            externalId=external_id,
+            calibration=calibration
+        )
+        resp = self.session.post(url, json=js, headers=self.headers)
+        return self._raise_on_error(resp).json()
+
+    def get_requests_for_request_ids(self, request_ids: List[int]):
+        url = f"{self.base_url}/v1/inputs/requests"
+        js = request_ids
+        resp = self.session.get(url, json=js, headers=self.headers)
+        return self._raise_on_error(resp).json()
+
+    def get_requests_for_input_lists(self, input_list_id: int):
+        url = f"{self.base_url}/v1/inputs/requests?inputListId={input_list_id}"
+        resp = self.session.get(url, headers=self.headers)
+        return self._raise_on_error(resp).json()
+
+    def get_input_lists_for_inputs(self, internal_ids: List[str]):
+        url = f"{self.base_url}/v1/inputs/input-lists"
+        js = internal_ids
+        resp = self.session.get(url, json=js, headers=self.headers)
+        return self._raise_on_error(resp).json()
+
+    def get_input_status(self, internal_ids: List[str]):
+        url = f"{self.base_url}/v1/inputs/export-status"
+        js = internal_ids
+        resp = self.session.get(url, json=js, headers=self.headers)
+        return self._raise_on_error(resp).json()
+
+    def download_annotations(self, internal_ids: List[str], request_id=None):
+        url = f"{self.base_url}/v1/inputs/export"
+        if request_id:
+            url += f"?requestId={request_id}"
+        js = internal_ids
+        resp = self.session.get(url, json=js, headers=self.headers)
+        return self._raise_on_error(resp).json()
---
+++ annotell-cloud-storage/annotell/cloud_storage/test_input_api.py
@@ -0,0 +1,103 @@
+import os
+import json
+from pathlib import Path
+# from annotell.cloud_storage.input_api_client import InputApiClient
+
+from input_api_client import InputApiClient
+
+
+if __name__ == "__main__":
+    c = InputApiClient(base_url="http://annotell.org:8010", api_token="DEF")
+
+    folder = Path("/Users/markocotra/Downloads/test_input_api")
+    files = [
+        "india_1259396926226198_1575361708215415_FC.jpg",
+        "india_1259396926226198.pcd"
+    ]
+
+    path_to_calibration = os.path.join(folder, 'annotell_sensor_config.json')
+
+    with open(path_to_calibration, 'r') as fp:
+        calibration = json.load(fp)
+
+    input_list_id = 8
+
+    scene_metadata = {
+        "externalId": "master_skywalker_what_are_we_going_to_do",
+        "sourceSpecification": {
+            "filesToSource": {
+                "india_1259396926226198_1575361708215415_FC.jpg": "FC",
+                "india_1259396926226198.pcd": "lidar"
+            },
+            "sourceToPrettyName": {
+                "FC": "Front Camera"
+            },
+            "sourceOrder": ["FC", "lidar"]
+        },
+        "calibrationSpec": {
+            "externalId": "big_boi_test:1337",
+            "calibration": calibration
+        }
+        # "calibrationId": 419
+    }
+    """
+    resp = c.create_inputs_for_files(folder, files, input_list_id, scene_metadata)
+    print(resp)
+
+    print()
+    resp = c.list_projects()
+    print(resp)
+
+    print()
+    resp = c.list_input_lists(7)
+    print(resp)
+
+    print()
+    resp = c.get_calibration_data()
+    print(resp)
+
+    print()
+    resp = c.get_calibration_data(id=428)
+    print(resp)
+
+    print()
+    resp = c.get_calibration_data(external_id="big_boi_test:1337")
+    print(resp)
+
+    print()
+    resp = c.create_calibration_data(calibration, external_id='We grant you a seat on this council')
+    print(resp)
+    """
+
+    """
+    print()
+    resp = c.mend_input_data()
+    print(resp)
+
+    print()
+    resp = c.invalidate_input()
+    print(resp)
+    """
+
+    print()
+    resp = c.get_requests_for_request_ids(request_ids=[23])
+    print(resp)
+
+    print()
+    resp = c.get_requests_for_input_lists(input_list_id=8)
+    print(resp)
+
+    print()
+    internal_ids = ["ed8cf0da-5f5a-4b6f-bfcf-684358b0ad50", "be25e986-5be3-4bc7-8d69-321b63899942"]
+    resp = c.get_input_lists_for_inputs(internal_ids=internal_ids)
+    print(resp)
+
+    print()
+    internal_ids = ["ed8cf0da-5f5a-4b6f-bfcf-684358b0ad50", "be25e986-5be3-4bc7-8d69-321b63899942", "cdefdf49-ca27-464a-b29d-976f72e63556"]
+    resp = c.get_input_status(internal_ids=internal_ids)
+    print(resp)
+
+    print()
+    internal_ids = ["ed8cf0da-5f5a-4b6f-bfcf-684358b0ad50", "be25e986-5be3-4bc7-8d69-321b63899942", "cdefdf49-ca27-464a-b29d-976f72e63556"]
+    resp = c.download_annotations(internal_ids=internal_ids, request_id=23)
+    print(resp)
