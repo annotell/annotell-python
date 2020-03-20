412b98c0e3114894aebe22df78b0c065da70d230
michel <michel.edkrantz@annotell.com>
2020-03-20 21:57:54 +0100

    use oauth for input_api_client

--- annotell-cloud-storage/annotell/cloud_storage/input_api_client.py
+++ annotell-cloud-storage/annotell/cloud_storage/input_api_client.py
@@ -1,29 +1,49 @@
-import os
-
 import requests
 import logging
-from typing import List, Mapping
+from typing import List, Mapping, Optional
 from pathlib import Path
 import mimetypes
 from . import __version__
+from annotell.auth.authsession import AuthSession, DEFAULT_HOST as DEFAULT_AUTH_HOST
 
-BASE_URL = "https://input.annotell.com"
+DEFAULT_HOST = "https://input.annotell.com"
 
 log = logging.getLogger(__name__)
+
 
 class InputApiClient:
     """Creates Annotell inputs from local files"""
 
-    def __init__(self, base_url, api_token=None):
-        self.api_token = api_token or os.getenv("ANNOTELL_API_TOKEN")
-        self.base_url = base_url
-        self.session = requests.Session()
+    def __init__(self, *,
+                 client_id: Optional[str] = None,
+                 client_secret: Optional[str] = None,
+                 api_token: Optional[str] = None,
+                 host: str = DEFAULT_HOST,
+                 auth_host: str = DEFAULT_AUTH_HOST):
+        """
+        :param client_id: client id for authentication
+        :param client_secret: client secret for authentication
+        :param api_token: legacy api token for authentication.
+        :param host: override for input api url
+        :param auth_host: override for authentication url
+        """
+
+        self.host = host
+
+        self.oauth_session = AuthSession(host=auth_host,
+                                         api_token=api_token,
+                                         client_id=client_id,
+                                         client_secret=client_secret)
+
         self.headers = {
-            "Authorization": f"Bearer {self.api_token}",
             "Accept-Encoding": "gzip",
             "Accept": "application/json",
             "User-Agent": f"annotell-cloud-storage:{__version__}"
         }
+
+    @property
+    def session(self):
+        return self.oauth_session.session
 
     def _raise_on_error(self, resp: requests.Response) -> requests.Response:
         try:
@@ -35,7 +55,7 @@ class InputApiClient:
 
     def get_upload_urls(self, files: List[str]):
         """Get upload urls to cloud storage"""
-        url = f"{self.base_url}/v1/inputs/upload-urls"
+        url = f"{self.host}/v1/inputs/upload-urls"
         js = dict(files=files)
         resp = self.session.get(url, json=js, headers=self.headers)
         return self._raise_on_error(resp).json()
@@ -55,11 +75,10 @@ class InputApiClient:
                     log.error(f"Got {resp.status_code} error calling cloud bucket upload, "
                               f"got response\n{resp.content}")
 
-
     def create_inputs(self, files: List[str], job_id: str):
         """Create inputs from uploaded files"""
         log.info(f"Creating inputs for files with job_id={job_id}")
-        url = f"{self.base_url}/v1/inputs"
+        url = f"{self.host}/v1/inputs"
         js = dict(files=files, jobId=job_id)
         resp = self.session.post(url, json=js, headers=self.headers)
         self._raise_on_error(resp)
@@ -73,4 +92,3 @@ class InputApiClient:
         self.upload_files(folder, items)
         self.create_inputs(files, job_id)
         return job_id
-
