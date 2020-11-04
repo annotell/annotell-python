468b496dc22661f9998d8c6e9d10a448d7e90ca9
Alexander Lydh <alexander.lydh@annotell.com>
2020-11-04 15:55:10 +0100

    Log response from spark emr

--- annotell-kpi/annotell/kpi/logging.py
+++ annotell-kpi/annotell/kpi/logging.py
@@ -6,7 +6,7 @@ def setup_logging() -> type(logging.log):
     FORMAT = '%(asctime)-15s %(levelname)-1s [%(name)-10s] %(message)s'
     logging.basicConfig(format=FORMAT, stream=sys.stdout)
     log = logging.getLogger(__name__)
-    log.setLevel(logging.INFO)
+    log.setLevel(logging.DEBUG)
     return log
 
 
