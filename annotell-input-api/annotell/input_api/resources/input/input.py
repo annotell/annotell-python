e4d82fafcf8ff89931353e194d28c626e87d167b
fredrik jacobson <fredrik.jacobson@annotell.com>
2021-01-22 11:19:09 +0100

    remove unused methods

--- annotell-input-api/annotell/input_api/resources/input/input.py
+++ annotell-input-api/annotell/input_api/resources/input/input.py
@@ -2,6 +2,7 @@ import logging
 from typing import List, Optional, Dict
 
 from annotell.input_api import model as IAM
+from annotell.input_api.util import filter_none
 from annotell.input_api.resources.abstract import InputAPIResource
 
 log = logging.getLogger(__name__)
@@ -11,50 +12,6 @@ class InputResource(InputAPIResource):
     """
     Class exposing Annotell Inputs
     """
-
-    def get_input_jobs_status(self, internal_ids: Optional[List[str]] = None,
-                              external_ids: Optional[List[str]] = None
-                              ) -> List[IAM.InputJob]:
-        """
-        Returns a list of input jobs, either:
-        * All input jobs connected to the given lists of internal and external ids
-        * All input jobs connected to the user organization, if no ids were given
-
-        :param internal_ids: List of internal ids
-        :param external_ids: List of external ids
-        :return List: List containing InputJob objects
-        """
-
-        if internal_ids is None:
-            internal_ids = []
-        if external_ids is None:
-            external_ids = []
-
-        js = dict(
-            internalIds=internal_ids,
-            externalIds=external_ids
-        )
-        json_resp = self.client.post("v1/inputs/job-status", json=js)
-
-        return [IAM.InputJob.from_json(js) for js in json_resp]
-
-    def count_inputs_for_external_ids(self, external_ids: List[str]) -> Dict[str, int]:
-        """
-        For each external id, returns a count of how many inputs exists with that external id.
-
-        :param external_ids: List of external ids
-        :return Dict: Dictionary which maps an external id to a count of inputs with that external id
-        """
-
-        if len(external_ids) == 0:
-            log.error("You need to specify a list of external ids.")
-            return
-
-        external_ids_csv = ",".join(external_ids)
-        json_resp = self.client.get("v1/inputs/count-for-ids", params={
-            "externalIds": external_ids_csv
-        })
-        return json_resp
 
     def get_internal_ids_for_external_ids(self, external_ids: List[str]) -> Dict[str, List[str]]:
         """
@@ -78,29 +35,21 @@ class InputResource(InputAPIResource):
         resp_json = self.client.post("v1/inputs/invalidate", json=invalidated_json)
         return IAM.InvalidatedInputsResponse.from_json(resp_json)
 
-    def remove_inputs_from_input_list(self, input_list_id: int, input_ids: List[int]):
-        """
-        Removes inputs from specified input list, without invalidating the input
-
-        :param input_list_id: The input list id where the inputs should be removed
-        :param input_ids: The input ids to remove
-        :return RemovedInputsResponse: Class containing what inputs were removed
-        """
-        removed_json = dict(inputListId=input_list_id, inputIds=input_ids)
-        resp_json = self.client.post("v1/inputs/remove", json=removed_json)
-        return IAM.RemovedInputsResponse.from_json(resp_json)
-
-    def get_inputs(self, project_id: int, invalidated: bool = False) -> List[IAM.Input]:
+    def get_inputs(self, project: str, batch: str, invalidated: bool = False, external_id: Optional[str] = None) -> List[IAM.Input]:
         """
         Gets inputs for project, with option to filter for invalidated inputs
 
-        :param project_id: Project id to filter
+        :param project: Project (identifier) to filter
+        :param batch: Batch (identifier) to filter
         :param invalidated: Returns invalidated inputs if True, otherwise valid inputs
+        :param external_id: External ID to filter input on
         :return List: List of Inputs
         """
 
-        json_resp = self.client.get("v1/inputs", params={
-            "projectId": project_id,
-            "invalidated": invalidated
-        })
+        json_resp = self.client.get("v1/inputs", params=filter_none({
+            "project": project,
+            "batch": batch,
+            "invalidated": invalidated,
+            "externalId": external_id
+        }))
         return [IAM.Input.from_json(js) for js in json_resp]
--- annotell-input-api/annotell/input_api/resources/project/project.py
+++ annotell-input-api/annotell/input_api/resources/project/project.py
@@ -9,20 +9,7 @@ class ProjectResource(InputAPIResource):
     Project related information
     """
 
-    def get_requests_for_project_id(self, project_id: int) -> List[IAM.Request]:
-        """
-        Returns all Requests connected to a project id
-
-        :param project_id: A project id
-        :return List: List containing Request objects
-        """
-        json_resp = self.client.get("v1/inputs/requests", params={
-            "projectId": project_id
-        })
-
-        return [IAM.Request.from_json(js) for js in json_resp]
-
-    def list_projects(self) -> List[IAM.Project]:
+    def get_projects(self) -> List[IAM.Project]:
         """
         Returns all projects connected to the users organization.
 
@@ -31,7 +18,7 @@ class ProjectResource(InputAPIResource):
         json_resp = self.client.get("v1/inputs/project")
         return [IAM.Project.from_json(js) for js in json_resp]
 
-    def list_project_batches(self, project: str) -> List[IAM.Project]:
+    def get_project_batches(self, project: str) -> List[IAM.Project]:
         """
         Returns all `batches` for the `project`.
 
@@ -39,16 +26,6 @@ class ProjectResource(InputAPIResource):
         """
         json_resp = self.client.get(f"v1/inputs/project/{project}/batch")
         return [IAM.InputBatch.from_json(js) for js in json_resp]
-
-    def list_input_lists(self, project_id: int) -> List[IAM.InputList]:
-        """
-        Returns a list of all input lists connected to the specified project.
-
-        :param project_id: The id of the project
-        :return List: List with the input lists connected to the project id
-        """
-        json_resp = self.client.get(f"v1/inputs/input-lists?projectId={project_id}")
-        return [IAM.InputList.from_json(js) for js in json_resp]
 
     def publish_batch(self, project: str, batch: str) -> IAM.InputBatch:
         """
